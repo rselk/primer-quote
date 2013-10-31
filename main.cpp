590a4187ac38695bef51349f18f9d6ad5b35f489
Ryan Selk <ryanselk@gmail.com>
2013-10-31 02:07:20 -0600

    add header comment

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/*
+   This is a program that is discussed in chapter 15 of the book C++ Primer.
+
+   The program focuses on teaching the following
+   - Virtual Functions & Pure Virtual Functions
+   - Object Orientated Programming
+   - Smart Pointers (shared_ptr in this case)
+   - Writing an abstract base class
+   
+   Compiled on G++ 4.7 on Ubuntu 13.04
+*/
+
 #include <iostream>
 #include <vector>
 #include <memory>
@@ -8,35 +20,19 @@
 
 int main()
 {
-
   std::vector<std::shared_ptr<Quote>> v1;
-
-  //single sales.
-  Quote base("0-201-82470-1", 50);
-  Quote base1("0-301-82470-1", 20);
-  Quote base2("0-401-82470-1", 90);
-  Bulk_quote bq("0-501-82470-1", 22, 40, .10); //discount applied at 40
-  Bulk_quote bq1("0-601-82470-1", 52, 20, .20);
-  Bulk_quote bq2("0-701-82470-1", 92, 90, .30);
   
-  v1.push_back(std::make_shared<Quote>(base));
-  v1.push_back(std::make_shared<Quote>(base1));
-  v1.push_back(std::make_shared<Quote>(base2));
+  v1.push_back(std::make_shared<Quote>("0-201-82470-1", 50));
+  v1.push_back(std::make_shared<Quote>("0-301-82470-1", 2));
+  v1.push_back(std::make_shared<Quote>("0-401-82470-1", 9));
   v1.push_back(std::make_shared<Bulk_quote>("0-701-82470-1", 92, 90, .30));
   v1.push_back(std::make_shared<Bulk_quote>("0-701-82470-1", 44, 40, .30));
   v1.push_back(std::make_shared<Bulk_quote>("0-701-82470-1", 22, 10, .30));
 
 
   double sum = 0.0;
-  std::cout<< "---------------------" <<std::endl;
-
   for (const auto &i : v1)
-  {
     sum += print_total(std::cout, *i, 1);
-  }
-
-  std::cout<< "---------------------" <<std::endl;
 
   std::cout<<"Total Sum: " << sum <<std::endl;
-
 }
